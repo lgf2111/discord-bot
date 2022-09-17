eb6173c1353c6162a56f3c5a3ca41b30c38f06c3
lgf2111 <lgf2111@gmail.com>
2022-09-17 20:05:36 +0800

    cannot debug

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-
-# from waitress import serve
+from waitress import serve
 from threading import Thread
 
 app = Flask(__name__)
 
 
 def run():
-    # serve(app, host="0.0.0.0", port=8000)
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    serve(app, host="0.0.0.0", port=8000)
 
 
 def thread_run():
